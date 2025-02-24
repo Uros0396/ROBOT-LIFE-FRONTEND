c3a1548e04b844ddd59104c100ce31b241db5d80
uros milenkovic <urosm4471@gmail.com>
2025-02-24 10:54:36 +0100

    main cleaned

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,3 @@
-/*import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import productReducer from "./ReducerComponent/getProductsReducer";
-import cartReducer from "./ReducerComponent/cartSlice";
-import commentReducer from "./ReducerComponent/commentSlice.js";
-import App from "./App.jsx";
-
-const reducer = combineReducers({
-  productSlice: productReducer,
-  cartSlice: cartReducer,
-  comments: commentReducer,
-});
-
-const store = configureStore({
-  reducer,
-});
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
-);
-
-export { store };*/
-
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
@@ -40,7 +10,6 @@ import cartReducer from "./ReducerComponent/cartSlice";
 import commentReducer from "./ReducerComponent/commentSlice.js";
 import App from "./App.jsx";
 
-// Carica la tua chiave pubblica Stripe
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 const reducer = combineReducers({
@@ -56,7 +25,6 @@ const store = configureStore({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      {/* Avvolgi App con Elements per Stripe */}
       <Elements stripe={stripePromise}>
         <App />
       </Elements>
